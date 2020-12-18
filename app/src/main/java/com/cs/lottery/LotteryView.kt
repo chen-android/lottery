79bfd770ce2923ebf026bdac5119f3d56a790c32
Administrator <1014988429@qq.com>
2020-12-18 17:05:19 +0800

    加球

--- app/src/main/java/com/cs/lottery/LotteryView.kt
+++ app/src/main/java/com/cs/lottery/LotteryView.kt
@@ -7,7 +7,9 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
-import kotlin.math.min
+import androidx.core.content.res.use
+import kotlin.math.*
+import kotlin.random.Random
 
 /**
  * @Author chenshuai
@@ -15,11 +17,60 @@ import kotlin.math.min
  */
 class LotteryView(context: Context, attributeSet: AttributeSet?) : View(context, attributeSet) {
     private val bgPaint: Paint = Paint()
+    private val ballPaint: Paint = Paint()
+    private val textPaint: Paint = Paint()
+    private val ballList: MutableList<Ball> = mutableListOf()
+    private var ballCount: Int = 8
+    private var ballRadius: Float = 20f
+    private var textSize: Float = 12f
+    private var diameter: Int = 0
+    private var bgRadius: Float = 0f
 
     init {
+        context.obtainStyledAttributes(attributeSet, R.styleable.LotteryView).use {
+            ballCount = it.getInteger(R.styleable.LotteryView_ballCount, 8)
+            ballRadius = it.getDimension(R.styleable.LotteryView_ballRadius, 40f)
+            textSize = it.getDimension(R.styleable.LotteryView_android_textSize, 40f)
+        }
+
         bgPaint.color = Color.rgb(99, 99, 99)
         bgPaint.style = Paint.Style.STROKE
         bgPaint.strokeWidth = 10f
+
+        ballPaint.style = Paint.Style.STROKE
+        ballPaint.strokeWidth = 4f
+
+        textPaint.textAlign = Paint.Align.CENTER
+        textPaint.textSize = textSize
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        ballList.clear()
+        diameter = min(w, h)
+        bgRadius = diameter / 2f
+        for (i in 1..ballCount) {
+            var x = 0f
+            var y = 0f
+            while (true) {
+                x = Random.nextInt(diameter).toFloat()
+                y = Random.nextInt(diameter).toFloat()
+                if (twoPointAway(x, y, bgRadius, bgRadius) > bgRadius - ballRadius) {
+                    continue
+                }
+                if (ballList.find { ball -> twoPointAway(x, y, ball.x, ball.y) < ballRadius * 2 } != null) {
+                    continue
+                }
+                break
+            }
+            ballList.add(
+                Ball(
+                    i.toString(), x, y,
+                    Color.rgb(Random.nextInt(255), Random.nextInt(255), Random.nextInt(255)),
+                    ballRadius, 10, Random.nextInt(360)
+                )
+            )
+        }
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -53,5 +104,37 @@ class LotteryView(context: Context, attributeSet: AttributeSet?) : View(context,
         super.onDraw(canvas)
         val radius = min(measuredWidth, measuredHeight) / 2f
         canvas?.drawCircle(radius, radius, radius, bgPaint)
+        ballList.forEach { ball ->
+            drawBall(canvas, ball)
+        }
+        ballList.forEach { ball ->
+            var targetX = ball.x + (ball.speed * cos(Math.toRadians(ball.angle.toDouble()))).toFloat()
+            var targetY = ball.x + (ball.speed * sin(Math.toRadians(ball.angle.toDouble()))).toFloat()
+            val twoPointAway = twoPointAway(targetX, targetY, bgRadius, bgRadius)
+            if (twoPointAway > bgRadius - ballRadius) {
+
+            }
+        }
     }
+
+    private fun drawBall(canvas: Canvas?, ball: Ball) {
+        ballPaint.color = ball.color
+        canvas?.drawCircle(ball.x, ball.y, ball.radius, ballPaint)
+        canvas?.drawText(ball.text, ball.x, ball.y + textSize * 2 / 5, textPaint)
+    }
+
+    private fun twoPointAway(x1: Float, y1: Float, x2: Float, y2: Float): Float {
+        return sqrt((x1 - x2).pow(2) + (y1 - y2).pow(2))
+    }
+
+
+    data class Ball(
+        val text: String,
+        var x: Float,
+        var y: Float,
+        val color: Int,
+        val radius: Float,
+        var speed: Int,
+        var angle: Int
+    )
 }
